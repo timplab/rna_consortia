788bede97f68d792bc155436225b9517b7c006fd
rrazaghi <rrazagh1@jhu.edu>
2019-05-25 13:34:42 -0400

    Add files via upload

---
+++ nanostat_analysis/nanostat_bam.sh
@@ -0,0 +1,4 @@
+NanoStat --bam ../alignments/NA12878-cDNA-1D.fail.dedup.fastq.hg38.minimap2.sorted.bam -n NA12878-cDNA-1D.fail.bam.nanostat -t 50
+NanoStat --bam ../alignments/NA12878-cDNA-1D.pass.dedup.fastq.hg38.minimap2.sorted.bam -n NA12878-cDNA-1D.pass.bam.nanostat -t 50
+NanoStat --bam ../alignments/NA12878-DirectRNA.pass.dedup.NoU.fastq.hg38.minimap2.sorted.bam -n NA12878-DirectRNA.pass.bam.nanostat -t 50
+NanoStat --bam ../alignments/NA12878-DirectRNA.fail.dedup.NoU.fastq.hg38.minimap2.sorted.bam -n NA12878-DirectRNA.fail.bam.nanostat -t 50
---
+++ nanostat_analysis/nanostat_fq.sh
@@ -0,0 +1,4 @@
+NanoStat --fastq ../fastq/NA12878-cDNA-1D.fail.dedup.fastq.gz -n NA12878-cDNA-1D.fail.nanostat -t 50
+NanoStat --fastq ../fastq/NA12878-DirectRNA.fail.dedup.fastq.gz -n NA12878-DirectRNA.fail.nanostat -t 50
+NanoStat --fastq ../fastq/NA12878-cDNA-1D.pass.dedup.fastq.gz -n NA12878-cDNA-1D.pass.nanostat -t 50
+NanoStat --fastq ../fastq/NA12878-DirectRNA.pass.dedup.fastq.gz -n NA12878-DirectRNA.pass.nanostat -t 50
